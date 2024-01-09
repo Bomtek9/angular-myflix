a5f8f27d8caef8cee24ee2f9ed4cee13c99b7112
Bomtek9 <clayduplantis9@gmail.com>
2024-01-08 18:20:33 -0600

    components

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+// src/app/user-profile/user-profile.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+  styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  user: any = {};
+  favoriteMovies: any[] = [];
 
+  constructor(private fetchApiData: FetchApiDataService) {}
+
+  ngOnInit(): void {
+    this.getUserProfile();
+    this.getFavoriteMovies();
+  }
+
+  getUserProfile(): void {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    if (user) {
+      this.fetchApiData.getOneUser(user.Username).subscribe((data: any) => {
+        this.user = data;
+      });
+    }
+  }
+
+  getFavoriteMovies(): void {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    if (user) {
+      this.fetchApiData
+        .getFavoriteMovies(user.Username)
+        .subscribe((data: any) => {
+          this.favoriteMovies = data;
+        });
+    }
+  }
+
+  // Add more functions as needed for editing user profile and managing favorite movies
 }
